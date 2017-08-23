d133cb06b2e071180e9e022c2df410091abe2d92
Clemens Fruhwirth <clemens@endorphin.org>
2017-08-23 16:21:16 +0200

    rawtherapee: 5.0-r1 -> 5.2
    Also clean-up obsolete files.

--- pkgs/applications/graphics/rawtherapee/default.nix
+++ pkgs/applications/graphics/rawtherapee/default.nix
@@ -3,14 +3,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "5.0-r1";
+  version = "5.2";
   name = "rawtherapee-" + version;
 
   src = fetchFromGitHub {
     owner = "Beep6581";
     repo = "RawTherapee";
-    rev = version + "-gtk3";
-    sha256 = "06v3ir5562yg4zk9z8kc8a7sw7da88193sizjlk74gh5d3smgr4q";
+    rev = version;
+    sha256 = "0i3cr3335bw8yxxzn6kcdx6ccinlnxzrdbgl3ld1kym1w2n5449k";
   };
 
   buildInputs = [
